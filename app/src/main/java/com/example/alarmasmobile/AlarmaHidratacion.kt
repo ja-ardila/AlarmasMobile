3759e66b73b02cfeb17e87c5bc094553b1e600a3
ja-ardila <ja.ardila2@uniandes.edu.co>
2024-09-24 21:28:22 -0500

    Primera versión pantalla alarma hidratacion

---
+++ app/src/main/java/com/example/alarmasmobile/AlarmaHidratacion.kt
@@ -0,0 +1,14 @@
+package com.example.alarmasmobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AlarmaHidratacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarma_hidratacion)
+    }
+}
--- app/src/main/java/com/example/alarmasmobile/MainActivity.kt
+++ app/src/main/java/com/example/alarmasmobile/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.alarmasmobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Encontrar el botón por su ID y establecer el onClickListener
+        val boton = findViewById<Button>(R.id.button_prueba)
+        boton.setOnClickListener {
+            // Crear un intent para cambiar a la segunda actividad
+            val intent = Intent(this, AlarmaHidratacion::class.java)
+            startActivity(intent)  // Iniciar la segunda actividad
         }
     }
 }
